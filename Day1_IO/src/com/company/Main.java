591dbc89b3710a9bda5fae5bafe1a961934374e8
Martins Leitarts <martins.leitarts@avalanchelabs.com>
2022-07-12 19:48:25 +0300

    Scanner added

--- Day1_IO/src/com/company/Main.java
+++ Day1_IO/src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int a = 3;
+        System.out.println(a);
+
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+        System.out.println(input);
     }
 }
