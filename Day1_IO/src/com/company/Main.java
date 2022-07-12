3176f2a18f5162839f20cb622f101010c134c69c
Martins Leitarts <martins.leitarts@avalanchelabs.com>
2022-07-12 21:10:16 +0300

    Task solved

--- Day1_IO/src/com/company/Main.java
+++ Day1_IO/src/com/company/Main.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(chooseString("rt","b","t"));
+    }
+
+    public static void sample1(){
         int a = 3;
         System.out.println(a);
 
@@ -12,5 +16,53 @@ public class Main {
 
         String input = sc.nextLine();
         System.out.println(input);
+    }
+
+    public static void sample2(){
+        int a = 3;
+        int b = 5;
+
+        String c = "abc";
+        //if(a == 4 && b == 5){
+        //if(a == 4 || b == 5){
+        if(c.equals("abc")){
+            System.out.println("Viii!");
+        }else if(c.equals("bca")){
+
+        }
+        else if(c.equals("bca2")){
+
+        }
+        else{
+
+        }
+
+        switch(c){
+            case "abc":
+                System.out.println("Viii!");
+                break;
+            case "bca":
+                System.out.println("Viii2");
+                break;
+            default:
+                System.out.println("aaa");
+                break;
+        }
+    }
+
+    public static int sample3(int a, int b){
+        return a + b;
+    }
+
+    public static String chooseString(String a, String b, String c){
+        
+        StringBuilder newABC = new StringBuilder();
+        newABC.append(a+b+c);
+
+        if(newABC.length() > 5 ){
+            return newABC.toString();
+        } else {
+            return newABC.reverse().toString();
+        }
     }
 }
