b475abc009917f53cc3b99eed4e911e12fa87d25
Martins Leitarts <21012644+TechbearCoding@users.noreply.github.com>
2022-10-08 11:49:26 +0300

    Removed comments

--- Day4_OOP/src/com/company/Main.java
+++ Day4_OOP/src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
 	    //StaticTest.test2();
         Car c1 = new Car();
-        //c1.drive();
+        c1.drive();
 
         Car c2 = new Car("White");
         System.out.println(c2);
@@ -21,7 +21,7 @@ public class Main {
             carList.get(i).setColor("white");
             System.out.println(carList.get(i));
         }
-        //c2.drive();
+        c2.drive();
 
         StaticTest.a = 3;
     }
